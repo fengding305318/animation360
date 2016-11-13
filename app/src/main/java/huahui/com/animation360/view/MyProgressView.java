b0cb824b0cf15aad116764cabe10e499b9ab8463
fengding <761004883@qq.com>
2016-11-13 22:59:20 +0800

    加速球的点击事件

--- app/src/main/java/huahui/com/animation360/view/MyProgressView.java
+++ app/src/main/java/huahui/com/animation360/view/MyProgressView.java
@@ -9,14 +9,17 @@ import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by fengding on 2016/11/13.
  */
 public class MyProgressView extends View {
-    private int width = 100;
-    private int height = 100;
+    private int width = 200;
+    private int height = 200;
     private Paint circlePaint;
     private Paint progressPaint;
     private Paint textPaint;
@@ -25,6 +28,7 @@ public class MyProgressView extends View {
     private Path path = new Path();
     private int progress = 50;
     private int max = 100;
+    private GestureDetector detector;
     public MyProgressView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
@@ -57,6 +61,29 @@ public class MyProgressView extends View {
 
         bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         bitmapCanvas = new Canvas(bitmap);
+        detector = new GestureDetector(new MyGestureDetectorListener());
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return detector.onTouchEvent(event);
+            }
+        });
+        setClickable(true);
+    }
+
+    class MyGestureDetectorListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Toast.makeText(getContext(), "双击啦", Toast.LENGTH_SHORT).show();
+            return super.onDoubleTap(e);
+        }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            Toast.makeText(getContext(), "单击啦", Toast.LENGTH_SHORT).show();
+            return super.onSingleTapConfirmed(e);
+        }
+
     }
 
     @Override
@@ -74,7 +101,7 @@ public class MyProgressView extends View {
         path.lineTo(width, height);
         path.lineTo(0, height);
         path.lineTo(0, y);
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 5; i++) {
             path.rQuadTo(10, -10, 20, 0);
             path.rQuadTo(10, 10, 20, 0);
         }
